6f6b8f85e875b77cf2c32f45b3c7ce15567c7e1d
David Brownell <db@DavidBrownell.com>
2019-02-27 18:40:21 -0800

    Added cmake v3.13.4 for Windows and Linux

--- Activate_custom.py
+++ Activate_custom.py
@@ -52,8 +52,43 @@ def GetCustomActions(
     cases, this is Bash on Linux systems and Batch or PowerShell on Windows systems.
     """
 
-    # TODO
-    return []
+    actions = []
+
+    for tool in ["cmake"]:
+        for version in ["v3.13.4"]:
+            for operating_system, hash in [
+                ("Windows", "CFB94E2E356F1E0CF8574B345798410BA1B98C3F5B8F8D568E87879811C2A9F1"),
+                ("Linux", "B786120D2D1741ABFF9E2E69B7B94139216CA800559F28707522658568CCB98F"),
+            ]:
+                if CurrentShell.CategoryName != operating_system:
+                    continue
+
+                tool_dir = os.path.join(_script_dir, "Tools", tool, version, operating_system)
+                assert os.path.isdir(tool_dir), tool_dir
+
+                if not actions:
+                    actions.append(CurrentShell.Commands.Message(""))
+
+                actions.append(
+                    CurrentShell.Commands.Call(
+                        'python "{script}" Verify "{tool} - {version}" "{dir}" {hash}'.format(
+                            script=os.path.join(
+                                os.getenv("DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL"),
+                                "RepositoryBootstrap",
+                                "SetupAndActivate",
+                                "AcquireBinaries.py",
+                            ),
+                            tool=tool,
+                            version=version,
+                            dir=tool_dir,
+                            hash=hash,
+                        ),
+                    ),
+                )
+
+        actions.append(CurrentShell.Commands.Message(""))
+
+    return actions
 
 
 # ----------------------------------------------------------------------
