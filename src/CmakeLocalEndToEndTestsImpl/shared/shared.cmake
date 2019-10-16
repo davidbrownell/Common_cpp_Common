b3f7bb49cde6dbb368aa2e940db60ec9aeabaae9
David Brownell <db@DavidBrownell.com>
2019-10-16 13:59:45 -0700

    Updated cmake templates to include file attributes

--- src/CmakeLocalEndToEndTestsImpl/shared/shared.cmake
+++ src/CmakeLocalEndToEndTestsImpl/shared/shared.cmake
@@ -1,20 +1,23 @@
 cmake_minimum_required(VERSION 3.5.0)
 
-# If this is being invoked within a .cmake file (rather than in CMakeLists.txt),
-# change this `macro` to a `function`.
+set(_shared_this_path ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "")
+
 function(Impl)
     # Defining a function here to introduce a new scope for local variables
     set(_project_name Shared)
-    set(_${_project_name}_version_major 1)
-    set(_${_project_name}_version_minor 2)
-    set(_${_project_name}_version_patch 3)
-    set(_${_project_name}_version ${_${_project_name}_version_major}.${_${_project_name}_version_minor}.${_${_project_name}_version_patch})
+    set(_version_major 1)                   # '1' in the release 1.2.3-alpha1+201910161322
+    set(_version_minor 2)                   # '2' in the release 1.2.3-alpha1+201910161322
+    set(_version_patch 3)                   # '3' in the release 1.2.3-alpha1+201910161322
+    set(_version_prerelease_info "b1")      # Optional 'alpha1' in the release 1.2.3-alpha1+201910161322
+    set(_version_build_info "20191016")     # Optional '201910161322' in the release 1.2.3-alpha1+201910161322
+
+    set(_version ${_version_major}.${_version_minor}.${_version_patch})
 
     # Alpha version components (which are supported in SemVer) present problems
     # for cmake when the version is provided inline. However, things work as expected
     # when setting the version as a property.
     project(${_project_name} LANGUAGES CXX)
-    set(PROJECT_VERSION ${_${_project_name}_version})
+    set(PROJECT_VERSION ${_version})
 
     set(CMAKE_CXX_STANDARD 17)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
@@ -32,33 +35,50 @@ function(Impl)
 
     include(CppCommon)
 
-    # All paths should be relative to `${_this_path}`.
-    get_filename_component(_this_path ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+    include(${_shared_this_path}/../lib/lib.cmake)
+    include(${_shared_this_path}/cmake/GenerateFileAttributes.cmake)
 
-    include(${_this_path}/../lib/lib.cmake)
+    generate_file_attributes(
+        _file_attribute_sources
+        NAME ${_project_name}
+        COMPANY_NAME MyCompany
+        VERSION_MAJOR ${_version_major}
+        VERSION_MINOR ${_version_minor}
+        VERSION_PATCH ${_version_patch}
+        VERSION_PRERELEASE_INFO ${_version_prerelease_info}
+        VERSION_BUILD_INFO ${_version_build_info}
+    )
+
 
     add_library(
         ${_project_name}
         SHARED
-        ${_this_path}/shared.cpp
-        ${_this_path}/shared.h
+        ${_shared_this_path}/shared.cpp
+        ${_shared_this_path}/shared.h
+        ${_file_attribute_sources}
     )
 
     set_target_properties(
         ${_project_name} PROPERTIES
-        VERSION ${_${_project_name}_version}
-        SOVERSION ${_${_project_name}_version_major}
-    )
-
-    target_link_libraries(
-        ${_project_name}
-        Lib
+        VERSION ${_version}
+        SOVERSION ${_version_major}
     )
 
     target_include_directories(
-        ${_project_name}
-        INTERFACE
-        ${_this_path}
+        ${_project_name} INTERFACE
+        ${_includes}
+        ${_shared_this_path}
+    )
+
+    target_link_directories(
+        ${_project_name} INTERFACE
+        ${_libs}
+        ${_shared_this_path}
+    )
+
+    target_link_libraries(
+        ${_project_name} PRIVATE
+        Lib
     )
 endfunction()
 
